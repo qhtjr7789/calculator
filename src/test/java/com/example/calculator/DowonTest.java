d774ebb1872686c6bd66f91233d4d7608abceaf1
Dowon Lee <edowon0623@gmail.com>
2019-08-21 11:26:19 +0900

    upload a new test case

--- src/test/java/com/example/calculator/DowonTest.java
+++ src/test/java/com/example/calculator/DowonTest.java
@@ -1,4 +1,14 @@
 package com.example.calculator;
 
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class DowonTest {
+    @Test
+    public void Test_정수나눗셈1() {
+        Calculator cal = new Calculator();
+        int result = cal.division(10, 2);
+        assertEquals(5, result);
+    }
 }
